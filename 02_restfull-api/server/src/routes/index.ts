3b606d5517c478adcdbc14125c2faed7999b2328
jp-knj <kenji.tomita1996@gmail.com>
2021-05-30 17:56:05 +0900

    :tada: Setup Middleware and schema

--- 02_restfull-api/server/src/routes/index.ts
+++ 02_restfull-api/server/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Express, Request, Response } from "express";
 import { createShortUrl, handleRedirect } from "../controller/shortUrl.controller";
+import validateResource from "../middleware/validateResource";
+import shortUrlSchema from "../schema/createShortUrl.shema";
 
 function routes(app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => {
     return res.send("Hello World")
   });
-  app.post('/api/url', createShortUrl);
+  app.post('/api/url', validateResource(shortUrlSchema), createShortUrl);
 
   app.get('/:shortId', handleRedirect);
 }
