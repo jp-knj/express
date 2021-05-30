829236b3371b98d5e599c8a775db57c53d723187
jp-knj <kenji.tomita1996@gmail.com>
2021-05-30 11:07:16 +0900

    :tada: Update Anything

---
+++ 01_restful-api/src/routes.ts
@@ -0,0 +1,18 @@
+import { Express, Request, Response } from "express";
+import { createUserSessionHandler, getUserSessionsHandler, invalidateUserSessionHandler } from "./controller/session.controller";
+import { createUserHandler } from "./controller/user.controller";
+import { validateRequest, requiresUser } from "./middleware";
+import { createUserSchema, createUserSessionSchema } from "./schema/user.shema";
+
+export default function(app: Express){
+  app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
+  app.post("/api/users", validateRequest(createUserSchema), createUserHandler);
+  app.post(
+    "/api/sessions",
+    validateRequest(createUserSessionSchema),
+    createUserSessionHandler
+  )
+
+  app.get("/api/sessions", requiresUser, getUserSessionsHandler);
+  app.delete("/api/sessions", requiresUser, invalidateUserSessionHandler);
+}
