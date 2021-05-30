e201e90a3748886dbbb5883529284ef2ce846868
jp-knj <kenji.tomita1996@gmail.com>
2021-05-30 15:31:09 +0900

    :+1: send short url

--- 02_restfull-api/server/src/app.ts
+++ 02_restfull-api/server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import config from "config";
 import routes from "./routes";
+import db from "./db";
 
 const app = express();
 app.use(express.json());
@@ -10,5 +11,6 @@ const port = config.get("port") as number;
 
 app.listen(port, () => {
   console.info(`Server listing at http://localhost:${port}`);
+  db();
   routes(app);
 });
--- 02_restfull-api/server/src/routes/index.ts
+++ 02_restfull-api/server/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Express, Request, Response } from "express";
+import { createShortUrl } from "../controller/shortUrl.controller";
+
 function routes(app: Express) {
-  app.get('/healthcheck', ( req: Request, res: Response ) => {
+  app.get('/healthcheck', (req: Request, res: Response) => {
     return res.send("Hello World")
- })
+  });
+  app.post('/api/url', createShortUrl);
 }
 
 export default routes;
