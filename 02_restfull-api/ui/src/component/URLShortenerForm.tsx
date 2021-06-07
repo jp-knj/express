4f4443d9d161c0a6470e6298603df6ba5a61426b
jp-knj <kenji.tomita1996@gmail.com>
2021-06-07 19:39:02 +0900

    :tada: Clean up file

--- 02_restfull-api/ui/src/component/URLShortenerForm.tsx
+++ 02_restfull-api/ui/src/component/URLShortenerForm.tsx
@@ -1,5 +1,18 @@
+import { Input, Button, Box, Heading, InputGroup } from "@chakra-ui/react";
+import axios from 'axios';
+import React, { useState } from 'react';
+
 function URLShortenerForm() {
-  return <div>Hi!</div>;
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    debugger;
+  }
+  return <Box pos="relative">
+    <form onSubmit={handleSubmit}>
+       <Button type="submit">Create Short URL</Button>
+    </form>
+  </Box>
 }
 
 export default URLShortenerForm;
