3bebe117e349a6b14b885d8c9182930e02535015
jp-knj <kenji.tomita1996@gmail.com>
2021-06-07 21:28:36 +0900

    :+1: Connect server to ui]

--- 02_restfull-api/ui/src/component/URLShortenerForm.tsx
+++ 02_restfull-api/ui/src/component/URLShortenerForm.tsx
@@ -1,17 +1,35 @@
 import { Input, Button, Box, Heading, InputGroup } from "@chakra-ui/react";
-import axios from 'axios';
-import React, { useState } from 'react';
+import axios from  "axios";
+import React, { useState } from "react";
+import { SERVER_ENDPOINT } from "../config";
 
 function URLShortenerForm() {
-
+  const [destination, setDestination] = useState();
+  const [shortUrl, setShortUrl] = useState<{
+    shortId: string;
+  } | null>(null);
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    setShortUrl(null);
+    const result = await axios
+      .post(`${SERVER_ENDPOINT}/api/url`, {
+        destination,
+      })
+      .then((resp) => resp.data);
     debugger;
+    setShortUrl(result);
   }
   return <Box pos="relative">
     <form onSubmit={handleSubmit}>
-       <Button type="submit">Create Short URL</Button>
+      <Input onChange={(e: any) => setDestination(e.target.value)}
+        placeholder="https::example.com"
+      />
+      <Button type="submit">Create Short URL</Button>
     </form>
+    {JSON.stringify(shortUrl)}
+    {shortUrl && (
+      <a href={`${SERVER_ENDPOINT}/${shortUrl?.shortId}`}>CLICK ME</a>
+    )}
   </Box>
 }
 
