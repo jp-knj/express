61d891cb3ce30a78a58b46c47773d66851456964
jp-knj <kenji.tomita1996@gmail.com>
2021-06-10 21:48:30 +0900

    :+1: Add Login function

--- 03_social-app/api/src/routes/auth.ts
+++ 03_social-app/api/src/routes/auth.ts
@@ -4,6 +4,10 @@ import log from "../logger";
 import bcrypt from "bcrypt";
 
 export default function (app: Express) {
+
+  // @description register new user
+  // @route       POST /api/auth/register
+  // @access      Public
   app.post("/api/auth/register",
     async (req: Request, res: Response) => {
       try {
@@ -21,10 +25,28 @@ export default function (app: Express) {
         // Save user and return respond
         const user = await registerNewUser.save();
         res.status(200).json(user);
-        } catch (e) {
-          log.error(e);
-          return res.status(400).send(e.errors);
+      } catch (e) {
+        log.error(e);
+        return res.status(400).send(e.errors);
       }
     }
   )
+
+  // @description login user
+  // @route       POST /api/auth/login
+  // @access      Public
+  app.post("/api/auth/login",
+    async (req: Request, res: Response) => {
+      try {
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(404).json("user not found");
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json("wrong password");
+        res.status(200).json(user)
+      } catch (e) {
+        log.error(e);
+        res.status(500).json(e);
+      }
+    }
+  );
 }
