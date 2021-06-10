d9005c74f68f269f30ef7be8dc91229d019efc3e
jp-knj <kenji.tomita1996@gmail.com>
2021-06-10 21:17:26 +0900

    :+1: Update register function

--- 03_social-app/api/src/routes/auth.ts
+++ 03_social-app/api/src/routes/auth.ts
@@ -1,17 +1,26 @@
 import { Express, Request, Response } from "express";
 import User from "../models/User"
 import log from "../logger";
+import bcrypt from "bcrypt";
+
 export default function (app: Express) {
   app.post("/api/auth/register",
     async (req: Request, res: Response) => {
-      const registerNewUser = await new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-      });
-        try {
-          const user = await registerNewUser.save();
-          res.status(200).json(user);
+      try {
+        // generated Hashed Password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        // create New User
+        const registerNewUser = await new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: hashedPassword,
+        });
+
+        // Save user and return respond
+        const user = await registerNewUser.save();
+        res.status(200).json(user);
         } catch (e) {
           log.error(e);
           return res.status(400).send(e.errors);
