850e3eae4f3ed509223d07f5e5d2d6c0948f7cd8
jp-knj <kenji.tomita1996@gmail.com>
2021-06-10 21:04:52 +0900

    :+1: Add register function

--- 03_social-app/api/src/routes/auth.ts
+++ 03_social-app/api/src/routes/auth.ts
@@ -1,15 +1,21 @@
 import { Express, Request, Response } from "express";
 import User from "../models/User"
+import log from "../logger";
 export default function (app: Express) {
-  app.get("/api/auth/register",
+  app.post("/api/auth/register",
     async (req: Request, res: Response) => {
-      const user = await new User({
-        username: "knj",
-        email: "knj@gmail.com",
-        password: "123456"
+      const registerNewUser = await new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
       });
-      await user.save();
-      res.send("ok");
+        try {
+          const user = await registerNewUser.save();
+          res.status(200).json(user);
+        } catch (e) {
+          log.error(e);
+          return res.status(400).send(e.errors);
+      }
     }
   )
 }
