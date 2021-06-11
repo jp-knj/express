24adaaf5283c430f0a3163e4abd7cd061e52776b
jp-knj <kenji.tomita1996@gmail.com>
2021-06-11 09:42:39 +0900

    :+1: Add follow function

--- 03_social-app/api/src/routes/users.ts
+++ 03_social-app/api/src/routes/users.ts
@@ -68,6 +68,27 @@ export default function (app: Express) {
   // @description follow a user
   // @route       POST /api/auth/login
   // @access      Public
+  app.put("/api/users/:id/follow",
+    async (req: Request, res: Response) => {
+      if (req.body.userId !== req.params.id) {
+        try {
+          const user = await User.findById(req.params.id);
+          const currentUser = await User.findById(req.body.userId);
+          if (!user.followers.includes(req.body.userId)) {
+            await user.updateOne({ $push: { followers: req.body.userId } });
+            await currentUser.updateOne({ $push: { followings: req.params.id } });
+            res.status(200).json("user has been followed");
+          } else {
+              res.status(403).json("you allready follow this user");
+            }
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } else {
+        res.status(403).json("you cant follow yourself");
+      }
+    }
+  );
 
   // @description unfollow a user
   // @route       POST /api/auth/login
