07aaf3e0b673a80d0d8b93ee90cc9068a77e849e
jp-knj <kenji.tomita1996@gmail.com>
2021-06-12 13:10:59 +0900

    :sparkles: Add unfollow function

--- 03_social-app/api/src/routes/users.ts
+++ 03_social-app/api/src/routes/users.ts
@@ -93,6 +93,27 @@ export default function (app: Express) {
   // @description unfollow a user
   // @route       POST /api/auth/login
   // @access      Public
+  app.put("/api/users/:id/unfollow",
+    async (req: Request, res: Response) => {
+      if (req.body.userId !== req.params.id) {
+        try {
+          const user = await User.findById(req.params.id);
+          const currentUser = await User.findById(req.body.userId);
+          if (user.followers.includes(req.body.userId)) {
+            await user.updateOne({ $pull: { followers: req.body.userId } });
+            await currentUser.updateOne({ $pull: { followings: req.params.id } });
+            res.status(200).json("user has been unfollowed");
+          } else {
+              res.status(403).json("you dont follow this user");
+            }
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } else {
+        res.status(403).json("you cant follow yourself");
+      }
+    }
+  );
 
   // @description login user
   // @route       POST /api/auth/login
