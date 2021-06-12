aeb61f0064ee79e20c3c6378b8203e3bb6a628e0
jp-knj <kenji.tomita1996@gmail.com>
2021-06-12 15:07:25 +0900

    :sparkles: Add update post fnc

--- 03_social-app/api/src/routes/post.ts
+++ 03_social-app/api/src/routes/post.ts
@@ -22,6 +22,21 @@ export default function (app: Express) {
   // @description update a post
   // @route       POST /api/auth/register
   // @access      Public
+  app.put("/api/posts/:id",
+    async (req: Request, res: Response) => {
+      try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+          await post.updateOne({ $set: req.body });
+          res.status(200).json("the post have been update");
+        } else {
+          res.status(403).json("you can update only your post")
+        }
+      } catch (e) {
+        res.status(500).json(e);
+      }
+    }
+  )
 
   // @description update a post
   // @route       POST /api/auth/register
