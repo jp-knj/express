3dcf40f8f1ce8de9151f28273b246c576778a4b3
jp-knj <kenji.tomita1996@gmail.com>
2021-06-14 07:15:45 +0900

    :sparkles: Update topbar for styles

--- 03_social-app/client/src/components/topbar/Topbar.tsx
+++ 03_social-app/client/src/components/topbar/Topbar.tsx
@@ -1,5 +1,5 @@
 import "./topbar.css"
-import { Search, Person } from "@material-ui/icons";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 
 export default function Topbar() {
   return (
@@ -9,19 +9,27 @@ export default function Topbar() {
       </div>
       <div className="topbar__body">
         <div className="searchbar">
-          <Search />
+          <Search className="searchbar__icon"/>
           <input placeholder="Search for" className="search__input"/>
         </div>
       </div>
       <div className="topbar__append">
         <div className="topbar__links">
-          <span className="topbar__link">Home Page</span>
+          <span className="topbar__link">Home</span>
           <span className="topbar__link">Timeline</span>
         </div>
         <div className="topbar__icons">
           <div className="topbar__item">
             <Person />
             <span className="topbar__badge">1</span>
+          </div>
+          <div className="topbar__item">
+            <Chat />
+            <span className="topbar__badge">2</span>
+          </div>
+          <div className="topbar__item">
+            <Notifications />
+            <span className="topbar__badge">3</span>
           </div>
         </div>
       </div>
