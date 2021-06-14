d38ba036aaeb3b7bc2e49d5db0009b8224cc7337
jp-knj <kenji.tomita1996@gmail.com>
2021-06-14 19:34:01 +0900

    [update] Add Sidebar

--- 03_social-app/client/src/components/sidebar/Sidebar.tsx
+++ 03_social-app/client/src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,60 @@
-import React from 'react';
 import "./sidebar.css";
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
+  Group,
+  Bookmark,
+  HelpOutline,
+  WorkOutline,
+  Event,
+  School,
+} from "@material-ui/icons";
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      sidebar
+      <div className="sidebar__container">
+        <ul className="sidebar__list">
+          <li className="sidebar__item">
+            <RssFeed className="sidebar__prepend"/>
+            <span className="sidebar__label">Feed</span>
+          </li>
+          <li className="sidebar__item">
+            <Chat className="sidebar__prepend"/>
+            <span className="sidebar__label">Chats</span>
+          </li>
+          <li className="sidebar__item">
+            <PlayCircleFilledOutlined className="sidebar__prepend"/>
+            <span className="sidebar__label">Videos</span>
+          </li>
+          <li className="sidebar__item">
+            <Group className="sidebar__prepend"/>
+            <span className="sidebar__label">Groups</span>
+          </li>
+          <li className="sidebar__item">
+            <Bookmark className="sidebar__prepend"/>
+            <span className="sidebar__label">Bookmarks</span>
+          </li>
+          <li className="sidebar__item">
+            <HelpOutline className="sidebar__prepend"/>
+            <span className="sidebar__label">Questions</span>
+          </li>
+          <li className="sidebar__item">
+            <WorkOutline className="sidebar__prepend"/>
+            <span className="sidebar__label">Jobs</span>
+          </li>
+          <li className="sidebar__item">
+            <Event className="sidebar__prepend"/>
+            <span className="sidebar__label">Events</span>
+          </li>
+          <li className="sidebar__item">
+            <School className="sidebar__prepend"/>
+            <span className="sidebar__label">Courses</span>
+          </li>
+        </ul>
+        <button className="sidebar__button">Show More</button>
+        <hr className="sidebar__border" />
+      </div>
     </div>
   );
 }
--- 03_social-app/client/src/pages/home/Home.tsx
+++ 03_social-app/client/src/pages/home/Home.tsx
@@ -1,13 +1,18 @@
-import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+import "./home.css";
 
 export default function Home() {
   return (
     <>
       <Topbar />
-      <Sidebar/>
       <div className="home__container">
-        <h3>Home content</h3>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
       </div>
     </>
   );
