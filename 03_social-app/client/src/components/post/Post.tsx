1145e5fd9c89e1e83603d333ef40f79264cfbe4c
jp-knj <kenji.tomita1996@gmail.com>
2021-06-16 08:44:58 +0900

    :sparkles: Create post

--- 03_social-app/client/src/components/post/Post.tsx
+++ 03_social-app/client/src/components/post/Post.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import { MoreVert } from "@material-ui/icons";
 import "./post.css";
 
 const Post = () => {
   return (
     <div className="post">
-      Post
+      <div className="post__container">
+        <div className="post__header">
+          <div className="post__prepend">
+            <img
+              className="post__auther-image"
+              src=""
+              alt=""
+            />
+            <span className="post__author">
+            </span>
+            <span className="post__date"></span>
+          </div>
+          <div className="post__append">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="post__body">
+          <span className="post__text"></span>
+          <img className="post_image" src="" alt="" />
+        </div>
+        <div className="post__footer">
+          <div className="post__prepend">
+            <img className="likeIcon" src="assets/like.png" alt="" />
+            <img className="likeIcon" src="assets/heart.png" alt="" />
+            <span className=""></span>
+          </div>
+          <div className="post__append">
+            <span className="post__comment"></span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
