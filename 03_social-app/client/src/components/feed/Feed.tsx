ee775d78f39073437f67cb4ade1b77ae1bcd1bee
jp-knj <kenji.tomita1996@gmail.com>
2021-06-18 00:15:11 +0900

    [update] Create Post Components

--- 03_social-app/client/src/components/feed/Feed.tsx
+++ 03_social-app/client/src/components/feed/Feed.tsx
@@ -2,12 +2,16 @@ import Share from "../share/Share";
 import Post from "../post/Post";
 import "./feed.css";
 
+import { Posts } from "../../Data";
+
 const Feed = () => {
   return (
     <div className="feed">
       <div className="feed__container">
         <Share />
-        <Post/>
+        {Posts.map((p) => (
+          <Post key={p.id} desc={p.desc} photo={p.photo} date={p.photo} like={p.like} comment={p.comment}/>
+        ))}
       </div>
     </div>
   );
--- 03_social-app/client/src/components/post/Post.tsx
+++ 03_social-app/client/src/components/post/Post.tsx
@@ -1,32 +1,43 @@
 import { MoreVert } from "@material-ui/icons";
 import "./post.css";
 
-const Post = () => {
+interface PostProps {
+  // id: string,
+  desc?: string,
+  photo: string,
+  date: string,
+  userId?: number,
+  like: number,
+  comment: number
+}
+
+const Post: React.FC<PostProps> = (props) => {
+  const {  desc, photo, date,like, comment} = props;
   return (
     <div className="post">
       <div className="post__container">
         <div className="post__header">
           <div className="post__prepend">
-            <img className="post__profile" src="/assets/person/1.jpeg" alt=""/>
-            <span className="post__username">kenji tomita</span>
-            <span className="post__date">08.01</span>
+            <img className="post__profile" src={photo} alt=""/>
+            <span className="post__username">Kenji Tomita</span>
+            <span className="post__date">{date}</span>
           </div>
           <div className="post__append">
             <MoreVert />
           </div>
         </div>
         <div className="post__body">
-          <span className="post__text">Hello World</span>
-          <img className="post__image" src="/assets/person/1.jpeg" alt="" />
+          <span className="post__text">{desc}</span>
+          <img className="post__image" src={photo} alt="" />
         </div>
         <div className="post__footer">
           <div className="post__append">
             <img className="likeIcon" src="assets/like.png" alt="" />
             <img className="likeIcon" src="assets/heart.png" alt="" />
-            <span className="post__likeCounter"> people like it</span>
+            <span className="post__likeCounter">{like} people like it</span>
           </div>
           <div className="post__append">
-            <span className="post__comment">15</span>
+            <span className="post__comment">{comment}</span>
           </div>
         </div>
       </div>
