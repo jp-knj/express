7439b3d44b0aa11c8de2391acd9947270fa4fd35
jp-knj <kenji.tomita1996@gmail.com>
2021-06-19 22:47:16 +0900

    [update] add profile page and styleing

---
+++ 03_social-app/client/src/pages/profile/index.tsx
@@ -0,0 +1,32 @@
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+export default function profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profile__sidebar">
+          <div className="profile__header">
+            <div className="profile__cover">
+              <img src="/assets/post/3.jpeg" alt="/assets/post/3.jpeg" className="profile__cover-image" />
+              <img src="/assets/person/7.jpeg" alt="/assets/person/3.jpeg" className="profile__profile-image" />
+            </div>
+            <div className="profile__info">
+              <h4 className="profile__username">Kenji Tomita</h4>
+              <span className="profile__desc"></span>
+            </div>
+          </div>
+          <div className="profile__body">
+          <Feed />
+          <Rightbar />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
