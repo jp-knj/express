03c577ba35af9135bbbab1bdff3bb9dcbb95363e
jp-knj <kenji.tomita1996@gmail.com>
2021-06-20 00:10:04 +0900

    :sparkles: Add register page

---
+++ 03_social-app/client/src/pages/register/index.tsx
@@ -0,0 +1,27 @@
+import "./register.css";
+
+export default function Register() {
+  return (
+    <div className="login">
+        <div className="login__container">
+          <div className="login__prepend">
+            <h3 className="login__logo">Lamasocial</h3>
+            <span className="login__desc">Connect with friends and the world around you on Lamasocial.</span>
+          </div>
+          <div className="login__append">
+            <div className="login__box">
+              <input placeholder="Username" className="login__input" />
+              <input placeholder="Email" className="login__input" />
+              <input placeholder="Password" className="login__input" />
+              <input placeholder="Password" className="login__input" />
+              <button className="login__button">Log In</button>
+              <span className="login__forgot">Forgot Password?</span>
+              <button className="login__register-button">
+                Create a New Account
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
