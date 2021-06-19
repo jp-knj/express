6d38da8c47d755b6e0ebd8a6bb39bf989aa8b44a
jp-knj <kenji.tomita1996@gmail.com>
2021-06-19 23:49:47 +0900

    :sparkles: Add Login page

--- 03_social-app/client/src/App.tsx
+++ 03_social-app/client/src/App.tsx
@@ -1,9 +1,10 @@
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile";
+import Login from "./pages/login";
 
 function App() {
   return (
-    <Profile />
+    <Login />
   );
 }
 
